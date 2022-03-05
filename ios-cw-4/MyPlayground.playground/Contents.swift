f8f62ed1d8d5448fb8cd55926b7cd2b67cdb5e31
yamank41 <100734289+yamank41@users.noreply.github.com>
2022-03-05 13:41:04 +0300

    حطيت print

---
+++ ios-cw-4/MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var name = "يمان"
+
+var hobby = "تنس الطاولة"
+
+var favouritenumber = 4
+
+print("السلام عليكم معاكم \(name) ورقمي المفضل هو \(favouritenumber) واحب \(hobby) ")
+
+var number1 = 16
+
+var height = 1.83
